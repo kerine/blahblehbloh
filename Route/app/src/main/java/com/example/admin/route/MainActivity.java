d578ff47bd151f6af7f18e1cd8f835e1ca6b9603
kerine <kerine000@gmail.com>
2015-09-13 18:34:20 +0800

    added camera

--- Route/app/src/main/java/com/example/admin/route/MainActivity.java
+++ Route/app/src/main/java/com/example/admin/route/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.admin.route;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void goTo_CameraActivity(View view) {
+        Intent myIntent = new Intent(this,CameraActivity.class);
+        startActivity(myIntent);
     }
 }
