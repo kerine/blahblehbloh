c50c0144cb895e6b2997a219c2285c15ab377285
kerine <kerine000@gmail.com>
2015-10-19 16:47:36 +0800

    added user input via points, coupled with notes and photo

--- Route/app/src/main/java/com/example/admin/route/MainActivity.java
+++ Route/app/src/main/java/com/example/admin/route/MainActivity.java
@@ -6,13 +6,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    double latStart, lngStart, latEnd, lngEnd;
+
+    String titleSent, notesStartSent, notesEndSent, path, pathEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            titleSent = extras.getString("titleSent");
+            notesStartSent = extras.getString("notesStartSent");
+            notesEndSent = extras.getString("notesEndSent");
+
+            path = extras.getString("path");
+            pathEnd = extras.getString("pathEnd");
+
+            latStart = extras.getDouble("latStart");
+            lngStart = extras.getDouble("lngStart");
+            latEnd = extras.getDouble("latEnd");
+            lngEnd = extras.getDouble("lngEnd");
+
+            Toast.makeText(this, titleSent + notesStartSent + notesEndSent, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, path + pathEnd, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, String.format("%.6f", latStart) + String.format("%.6f", lngStart) + String.format("%.6f", latEnd) + String.format("%.6f", lngEnd), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
